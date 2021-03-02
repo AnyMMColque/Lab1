fe9129f65ceed82c500c8b386086c9609b7ea676
Any <anym.colque@gmail.com>
2021-03-02 00:17:42 -0400

    Problema 2

--- test.js
+++ test.js
@@ -3,4 +3,12 @@ let cad1=new cadena1("hola");
 //PROBLEMA 1
 let P1=["aa","ab","abc","abcd"];
 console.log(manager.longCadenaT(P1));
+//PROBLEMA 2
+//let P2="(bar)";
+//let P2="foo(bar)baz";
+//let P2="foo(bar)baz(blim)";
+//let P2="foo(bar(baz))blim";
+//let P2="foo(bar(baz(HOLAA)))ewew";
+//let P2="foo(bar(baz(HOLAA)))ewew";
+let P2="foo(bar(baz(HOLAA)))ewew(asd)(dsa)2s(BF)(iu)";
 
